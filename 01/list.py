2b77583b9fb7f71fff2636fd42374b9bc10a6b23
lvbing <lvbingwm@gmail.com>
2020-09-25 21:01:07 +0800

    add generator

--- 01/list.py
+++ 01/list.py
@@ -41,3 +41,29 @@ elif trim('    ') != '':
     print('测试失败!')
 else:
     print('测试成功!')
+
+
+def findMinAndMax(l_list):
+    if not l_list:
+        return None, None
+    min_num = l_list[0]
+    max_num = l_list[0]
+    for l in l_list:
+        if l < min_num:
+            min_num = l
+        if l > max_num:
+            max_num = l
+    return min_num, max_num
+
+
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
