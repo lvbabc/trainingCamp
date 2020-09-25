dbbb3584cca4114a040f0191c38e5899ca77e833
lvbing <lvbingwm@gmail.com>
2020-09-25 18:04:49 +0800

    add list

---
+++ 01/list.py
@@ -0,0 +1,43 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+
+fruits.count('tangerine')
+
+fruits.index('banana')
+
+fruits.index('banana', 4)  # Find next banana starting a position 4
+
+fruits.reverse()
+
+fruits.append('grape')
+
+fruits.sort()
+
+fruits.pop()
+
+
+def trim(s):
+    if s == '':
+        return s
+    elif s[0] == ' ':
+        return trim(s[1:])
+    elif s[-1] == ' ':
+        return trim(s[:-1])
+    else:
+        return s
+
+
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
