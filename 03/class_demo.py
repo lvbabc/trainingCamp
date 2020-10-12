ec623101aa191991399ff996d0bbb5ad0ff51776
lvbing <lvbingwm@gmail.com>
2020-10-12 10:46:14 +0800

    update class

--- 03/class_demo.py
+++ 03/class_demo.py
@@ -19,3 +19,36 @@ while x.counter < 10:
 print(x.counter)
 del x.counter
 
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('{0}: {1}'.format(self.name, self.score))
+
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+bart.print_score()
+lisa.print_score()
+
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+    def eat(self):
+        print('Eating meat...')
+
+
+class Cat(Animal):
+    pass
+
